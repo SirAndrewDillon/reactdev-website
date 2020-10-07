d2e06b47197424026b1825abc31bacd882ff8ad8
Andrew Dillon <majik895@gmail.com>
2020-10-07 12:18:00 -0400

    added images

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './ui/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>HELLO</h1>
+    </div>
+  );
+}
+
+export default App;
